6f27a4434e2be2547169e2a614ab681325a2dda7
GabrielaLukacova <gabluk01@easv365.dk>
2026-01-08 02:24:30 +0100

    chore(backend): enforce prettier via eslint

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['**/dist/**', '**/build/**', '**/node_modules/**'],
+  env: { es2022: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
